77a4dc029b0a92c09099e9f7da0646892e1e2897
Tom <15022636+Thaesch@users.noreply.github.com>
2024-01-28 15:18:23 +0100

    BG Music

--- GlobalGameJam24/Assets/by_tom/GameOver/OutroAudioHandler.cs
+++ GlobalGameJam24/Assets/by_tom/GameOver/OutroAudioHandler.cs
@@ -10,15 +10,15 @@ public class OutroAudioHandler : MonoBehaviour
         switch(gameOverState)
         {
             case GameOverState.Failure:
-                GameContext.Instance.AudioPlayer.RequestBGMusic(8); // -> When Music is in the game, uncomment
+                GameContext.Instance.AudioPlayer.RequestBGMusic(0); // -> When Music is in the game, uncomment
                 GameContext.Instance.AudioPlayer.RequestSFX(SFXType.Cry); // -> When SoundEffects are in the game, uncomment
                 break;
             case GameOverState.Sadness:
-                GameContext.Instance.AudioPlayer.RequestBGMusic(8);//  -> When Music is in the game, uncomment
+                GameContext.Instance.AudioPlayer.RequestBGMusic(0);//  -> When Music is in the game, uncomment
                 GameContext.Instance.AudioPlayer.RequestSFX(SFXType.Grumbling);//  -> When SoundEffects are in the game, uncomment
                 break;
             case GameOverState.Laughter:
-                GameContext.Instance.AudioPlayer.RequestBGMusic(8); // -> When Music is in the game, uncomment
+                GameContext.Instance.AudioPlayer.RequestBGMusic(0); // -> When Music is in the game, uncomment
                 GameContext.Instance.AudioPlayer.RequestSFX(SFXType.Laugh); // -> When SoundEffects are in the game, uncomment
                 break;
             default: break;
